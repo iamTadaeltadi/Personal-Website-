5de1880f811d735ec6465ae5e8514985057459be
Tadaeel Shewarega <109872251+iamTadaeltadi@users.noreply.github.com>
2023-09-27 17:14:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:"/vite-deploy/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Specify manual chunks based on your project's needs
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+}
+)
+
+  
